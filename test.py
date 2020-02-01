6021c92d01a727d7705f48ec48ff07e4af750b8c
Dominik Lewy <dominik.lewy@lingarogroup.com>
2020-02-01 18:05:56 +0100

    data augmentation added

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 from utils_experiments_multi_gpu import pascal_experiment
 
-experiment_desc = "../results/mgpu_regularization_l1_001"
-experiment = "../experiments/exp_50k_no_wrong.pkl"
-path = '../PascalVOC/img/'
-batch_size = 64*4
-epochs = 20
+experiment_desc = "../../PascalVOC_INSTA_results/local"
+experiment = "../exp_50k_no_wrong.pkl"
+path = '../../PascalVOC_INSTA_50k/img2/'
+batch_size = 4
+epochs = 1
 path_to_model = None
 base_model_trainable = True
 l2_regularization = 0.01
 learning_rate = 0.001
+augment = True
 
-pascal_experiment(experiment_desc, experiment, path, batch_size, epochs, base_model_trainable, path_to_model, l2_regularization, learning_rate)
+pascal_experiment(experiment_desc, experiment, path, batch_size, epochs, base_model_trainable, path_to_model, l2_regularization, learning_rate, augment)
--- utils_experiments_multi_gpu.py
+++ utils_experiments_multi_gpu.py
@@ -7,7 +7,8 @@ import os
 import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
-from keras.utils import multi_gpu_model
+import imgaug as ia
+from imgaug import augmenters as iaa
 
 dict_class_num =  {'aeroplane'     :0,
                    'bicycle'       :1,
@@ -30,15 +31,10 @@ dict_class_num =  {'aeroplane'     :0,
                    'table'         :18,
                    'trains'        :19}
 
-def matrix_image_loading(path):
-    img = Image.open(path)
-    img = np.array(img)/255
-    return(img)
-
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
     def __init__(self, list_IDs, labels, path, batch_size=32, dim=(32,32,32), n_channels=1,
-                 n_classes=10, shuffle=True):
+                 n_classes=10, shuffle=True, augment = False):
         'Initialization'
         self.dim = dim
         self.batch_size = batch_size
@@ -49,6 +45,7 @@ class DataGenerator(keras.utils.Sequence):
         self.shuffle = shuffle
         self.on_epoch_end()
         self.path = path
+        self.augment = augment
 
     def __len__(self):
         'Denotes the number of batches per epoch'
@@ -84,12 +81,108 @@ class DataGenerator(keras.utils.Sequence):
             # Store sample
             file_name = ID.split("_")[1]
             
-            X[i,] = matrix_image_loading(self.path + file_name)
+            X[i,] = self.matrix_image_loading(self.path + file_name)
 
             # Store class
             y[i] = dict_class_num[self.labels[ID]]
 
         return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+        
+    def matrix_image_loading(self, path):
+        img = Image.open(path)
+        img = np.array(img)#/255
+        if self.augment == True:
+           img = self.augmentor(img)
+        img = np.array(img)/255
+        return img
+        
+    def augmentor(self, images):
+        sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+        seq = iaa.Sequential(
+                [
+                # apply the following augmenters to most images
+                iaa.Fliplr(0.5),  # horizontally flip 50% of all images
+                iaa.Flipud(0.2),  # vertically flip 20% of all images
+                sometimes(iaa.Affine(
+                    scale={"x": (0.9, 1.1), "y": (0.9, 1.1)},
+                    # scale images to 80-120% of their size, individually per axis
+                    translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)},
+                    # translate by -20 to +20 percent (per axis)
+                    rotate=(-10, 10),  # rotate by -45 to +45 degrees
+                    shear=(-5, 5),  # shear by -16 to +16 degrees
+                    order=[0, 1],
+                    # use nearest neighbour or bilinear interpolation (fast)
+                    cval=(0, 255),  # if mode is constant, use a cval between 0 and 255
+                    mode=ia.ALL
+                    # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+                )),
+                # execute 0 to 5 of the following (less important) augmenters per image
+                # don't execute all of them, as that would often be way too strong
+                iaa.SomeOf((0, 5),
+                        [sometimes(iaa.Superpixels(p_replace=(0, 1.0),
+                                                            n_segments=(20, 200))),
+                            # convert images into their superpixel representation
+                            iaa.OneOf([
+                                    iaa.GaussianBlur((0, 1.0)),
+                                    # blur images with a sigma between 0 and 3.0
+                                    iaa.AverageBlur(k=(3, 5)),
+                                    # blur image using local means with kernel sizes between 2 and 7
+                                    iaa.MedianBlur(k=(3, 5)),
+                                    # blur image using local medians with kernel sizes between 2 and 7
+                            ]),
+                            iaa.Sharpen(alpha=(0, 1.0), lightness=(0.9, 1.1)),
+                            # sharpen images
+                            iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)),
+                            # emboss images
+                            # search either for all edges or for directed edges,
+                            # blend the result with the original image using a blobby mask
+                            iaa.SimplexNoiseAlpha(iaa.OneOf([
+                                    iaa.EdgeDetect(alpha=(0.5, 1.0)),
+                                    iaa.DirectedEdgeDetect(alpha=(0.5, 1.0),
+                                                            direction=(0.0, 1.0)),
+                            ])),
+                            iaa.AdditiveGaussianNoise(loc=0,
+                                                        scale=(0.0, 0.01 * 255),
+                                                        per_channel=0.5),
+                            # add gaussian noise to images
+                            iaa.OneOf([
+                                    iaa.Dropout((0.01, 0.05), per_channel=0.5),
+                                    # randomly remove up to 10% of the pixels
+                                    iaa.CoarseDropout((0.01, 0.03),
+                                                        size_percent=(0.01, 0.02),
+                                                        per_channel=0.2),
+                            ]),
+                            iaa.Invert(0.01, per_channel=True),
+                            # invert color channels
+                            iaa.Add((-2, 2), per_channel=0.5),
+                            # change brightness of images (by -10 to 10 of original value)
+                            iaa.AddToHueAndSaturation((-1, 1)),
+                            # change hue and saturation
+                            # either change the brightness of the whole image (sometimes
+                            # per channel) or change the brightness of subareas
+                            iaa.OneOf([
+                                    iaa.Multiply((0.9, 1.1), per_channel=0.5),
+                                    iaa.FrequencyNoiseAlpha(
+                                            exponent=(-1, 0),
+                                            first=iaa.Multiply((0.9, 1.1),
+                                                                per_channel=True),
+                                            second=iaa.ContrastNormalization(
+                                                    (0.9, 1.1))
+                                    )
+                            ]),
+                            sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5),
+                                                                sigma=0.25)),
+                            # move pixels locally around (with random strengths)
+                            sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))),
+                            # sometimes move parts of the image around
+                            sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
+                        ],
+                        random_order=True
+                        )
+                ],
+                random_order=True
+        )
+        return seq.augment_image(images)
         
 
 def net_architecture(path_to_model, base_model_trainable, classes):
@@ -104,7 +197,7 @@ def net_architecture(path_to_model, base_model_trainable, classes):
         model.load_weights(path_to_model)
     return(model)
 
-def pascal_experiment(experiment_desc, experiment, path, batch_size, epochs, base_model_trainable, path_to_model, l2_regularization, learning_rate):
+def pascal_experiment(experiment_desc, experiment, path, batch_size, epochs, base_model_trainable, path_to_model, l2_regularization, learning_rate, augment):
 
     if not os.path.exists(experiment_desc):
         os.makedirs(experiment_desc)
@@ -126,7 +219,7 @@ def pascal_experiment(experiment_desc, experiment, path, batch_size, epochs, bas
     labels = {key: value for (key, value) in zip(list(data), [x.split("_")[0] for x in list(data)])}
 
     # Generators
-    training_generator = DataGenerator(partition['train'], labels, path, **params)
+    training_generator = DataGenerator(partition['train'], labels, path, augment=True, **params)
     validation_generator = DataGenerator(partition['validation'], labels, path, **params)
 
     # Design model
