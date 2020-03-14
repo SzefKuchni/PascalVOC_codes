e7e46b462f0f5d7f60199de9c45063527d52d8dc
Dominik Lewy <dominik.lewy@gmail.com>
2020-03-14 17:45:35 +0100

    stable code: multi gpu sequential experiments

--- test.py
+++ test.py
@@ -1,14 +1,243 @@
 from utils_experiments_multi_gpu import pascal_experiment
 
-experiment_desc = "../../PascalVOC_INSTA_results/local"
-experiment = "../exp_50k_no_wrong.pkl"
-path = '../../PascalVOC_INSTA_50k/img2/'
-batch_size = 4
-epochs = 1
+# Global arguments
+experiment = "../experiments/exp_50k_no_wrong.pkl"
+path = '../PascalVOC/img/'
+batch_size = 4*64
+epochs = 20
 path_to_model = None
 base_model_trainable = True
+
+# EXPERIMENT 1
+# Local arguments
+experiment_desc = "../results/mgpu_l2_005"
+l2_regularization = 0.005
+learning_rate = 0.001
+augment = False
+scale = (0.8, 1.2)
+translate_percent = (-0.2, 0.2)
+rotate = (-45, 45)
+shear = (-16,16)
+
+pascal_experiment(  experiment_desc, 
+                    experiment, 
+                    path, 
+                    batch_size, 
+                    epochs, 
+                    base_model_trainable, 
+                    path_to_model, 
+                    l2_regularization, 
+                    learning_rate, 
+                    augment, 
+                    scale, 
+                    translate_percent, 
+                    rotate, 
+                    shear)
+                    
+# EXPERIMENT 2
+# Local arguments
+experiment_desc = "../results/mgpu_l2_05"
+l2_regularization = 0.05
+learning_rate = 0.001
+augment = False
+scale = (0.8, 1.2)
+translate_percent = (-0.2, 0.2)
+rotate = (-45, 45)
+shear = (-16,16)
+
+pascal_experiment(  experiment_desc, 
+                    experiment, 
+                    path, 
+                    batch_size, 
+                    epochs, 
+                    base_model_trainable, 
+                    path_to_model, 
+                    l2_regularization, 
+                    learning_rate, 
+                    augment, 
+                    scale, 
+                    translate_percent, 
+                    rotate, 
+                    shear)
+                    
+# EXPERIMENT 3
+# Local arguments
+experiment_desc = "../results/mgpu_l2_001_DA_rotate_15_scale_02_translate_02"
 l2_regularization = 0.01
 learning_rate = 0.001
 augment = True
+scale = (0.8, 1.2)
+translate_percent = (-0.2, 0.2)
+rotate = (-15, 15)
+shear = (-16,16)
 
-pascal_experiment(experiment_desc, experiment, path, batch_size, epochs, base_model_trainable, path_to_model, l2_regularization, learning_rate, augment)
+pascal_experiment(  experiment_desc, 
+                    experiment, 
+                    path, 
+                    batch_size, 
+                    epochs, 
+                    base_model_trainable, 
+                    path_to_model, 
+                    l2_regularization, 
+                    learning_rate, 
+                    augment, 
+                    scale, 
+                    translate_percent, 
+                    rotate, 
+                    shear)
+                    
+# EXPERIMENT 4
+# Local arguments
+experiment_desc = "../results/mgpu_l2_001_DA_rotate_30_scale_02_translate_02"
+l2_regularization = 0.01
+learning_rate = 0.001
+augment = True
+scale = (0.8, 1.2)
+translate_percent = (-0.2, 0.2)
+rotate = (-30, 30)
+shear = (-16,16)
+
+pascal_experiment(  experiment_desc, 
+                    experiment, 
+                    path, 
+                    batch_size, 
+                    epochs, 
+                    base_model_trainable, 
+                    path_to_model, 
+                    l2_regularization, 
+                    learning_rate, 
+                    augment, 
+                    scale, 
+                    translate_percent, 
+                    rotate, 
+                    shear)
+                    
+# EXPERIMENT 5
+# Local arguments
+experiment_desc = "../results/mgpu_l2_001_DA_rotate_45_scale_02_translate_02"
+l2_regularization = 0.01
+learning_rate = 0.001
+augment = True
+scale = (0.8, 1.2)
+translate_percent = (-0.2, 0.2)
+rotate = (-45, 45)
+shear = (-16,16)
+
+pascal_experiment(  experiment_desc, 
+                    experiment, 
+                    path, 
+                    batch_size, 
+                    epochs, 
+                    base_model_trainable, 
+                    path_to_model, 
+                    l2_regularization, 
+                    learning_rate, 
+                    augment, 
+                    scale, 
+                    translate_percent, 
+                    rotate, 
+                    shear)
+                    
+# EXPERIMENT 6
+# Local arguments
+experiment_desc = "../results/mgpu_l2_001_DA_rotate_30_scale_01_translate_02"
+l2_regularization = 0.01
+learning_rate = 0.001
+augment = True
+scale = (0.9, 1.1)
+translate_percent = (-0.2, 0.2)
+rotate = (-30, 30)
+shear = (-16,16)
+
+pascal_experiment(  experiment_desc, 
+                    experiment, 
+                    path, 
+                    batch_size, 
+                    epochs, 
+                    base_model_trainable, 
+                    path_to_model, 
+                    l2_regularization, 
+                    learning_rate, 
+                    augment, 
+                    scale, 
+                    translate_percent, 
+                    rotate, 
+                    shear)
+                    
+# EXPERIMENT 7
+# Local arguments
+experiment_desc = "../results/mgpu_l2_001_DA_rotate_30_scale_03_translate_02"
+l2_regularization = 0.01
+learning_rate = 0.001
+augment = True
+scale = (0.7, 1.3)
+translate_percent = (-0.2, 0.2)
+rotate = (-30, 30)
+shear = (-16,16)
+
+pascal_experiment(  experiment_desc, 
+                    experiment, 
+                    path, 
+                    batch_size, 
+                    epochs, 
+                    base_model_trainable, 
+                    path_to_model, 
+                    l2_regularization, 
+                    learning_rate, 
+                    augment, 
+                    scale, 
+                    translate_percent, 
+                    rotate, 
+                    shear)
+                    
+# EXPERIMENT 8
+# Local arguments
+experiment_desc = "../results/mgpu_l2_001_DA_rotate_30_scale_02_translate_01"
+l2_regularization = 0.01
+learning_rate = 0.001
+augment = True
+scale = (0.2, 1.2)
+translate_percent = (-0.1, 0.1)
+rotate = (-30, 30)
+shear = (-16,16)
+
+pascal_experiment(  experiment_desc, 
+                    experiment, 
+                    path, 
+                    batch_size, 
+                    epochs, 
+                    base_model_trainable, 
+                    path_to_model, 
+                    l2_regularization, 
+                    learning_rate, 
+                    augment, 
+                    scale, 
+                    translate_percent, 
+                    rotate, 
+                    shear)
+                    
+# EXPERIMENT 9
+# Local arguments
+experiment_desc = "../results/mgpu_l2_001_DA_rotate_30_scale_02_translate_03"
+l2_regularization = 0.01
+learning_rate = 0.001
+augment = True
+scale = (0.2, 1.2)
+translate_percent = (-0.3, 0.3)
+rotate = (-30, 30)
+shear = (-16,16)
+
+pascal_experiment(  experiment_desc, 
+                    experiment, 
+                    path, 
+                    batch_size, 
+                    epochs, 
+                    base_model_trainable, 
+                    path_to_model, 
+                    l2_regularization, 
+                    learning_rate, 
+                    augment, 
+                    scale, 
+                    translate_percent, 
+                    rotate, 
+                    shear)
--- utils_experiments_multi_gpu.py
+++ utils_experiments_multi_gpu.py
@@ -34,7 +34,7 @@ dict_class_num =  {'aeroplane'     :0,
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
     def __init__(self, list_IDs, labels, path, batch_size=32, dim=(32,32,32), n_channels=1,
-                 n_classes=10, shuffle=True, augment = False):
+                 n_classes=10, shuffle=True, augment = False, scale = (0.8, 1.2), translate_percent = (-0.2, 0.2), rotate = (-45, 45), shear = (-16,16)):
         'Initialization'
         self.dim = dim
         self.batch_size = batch_size
@@ -46,6 +46,10 @@ class DataGenerator(keras.utils.Sequence):
         self.on_epoch_end()
         self.path = path
         self.augment = augment
+        self.scale = scale
+        self.translate_percent = translate_percent
+        self.rotate = rotate
+        self.shear = shear
 
     def __len__(self):
         'Denotes the number of batches per epoch'
@@ -102,83 +106,20 @@ class DataGenerator(keras.utils.Sequence):
                 [
                 # apply the following augmenters to most images
                 iaa.Fliplr(0.5),  # horizontally flip 50% of all images
-                iaa.Flipud(0.2),  # vertically flip 20% of all images
+                #iaa.Flipud(0.2),  # vertically flip 20% of all images
                 sometimes(iaa.Affine(
-                    scale={"x": (0.9, 1.1), "y": (0.9, 1.1)},
+                    scale={"x": self.scale, "y": self.scale},
                     # scale images to 80-120% of their size, individually per axis
-                    translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)},
+                    translate_percent={"x": self.translate_percent, "y": self.translate_percent},
                     # translate by -20 to +20 percent (per axis)
-                    rotate=(-10, 10),  # rotate by -45 to +45 degrees
-                    shear=(-5, 5),  # shear by -16 to +16 degrees
+                    rotate=self.rotate,  # rotate by -45 to +45 degrees
+                    shear=self.shear,  # shear by -16 to +16 degrees
                     order=[0, 1],
                     # use nearest neighbour or bilinear interpolation (fast)
-                    cval=(0, 255),  # if mode is constant, use a cval between 0 and 255
+                    #cval=(0, 255),  # if mode is constant, use a cval between 0 and 255
                     mode=ia.ALL
                     # use any of scikit-image's warping modes (see 2nd image from the top for examples)
                 )),
-                # execute 0 to 5 of the following (less important) augmenters per image
-                # don't execute all of them, as that would often be way too strong
-                iaa.SomeOf((0, 5),
-                        [sometimes(iaa.Superpixels(p_replace=(0, 1.0),
-                                                            n_segments=(20, 200))),
-                            # convert images into their superpixel representation
-                            iaa.OneOf([
-                                    iaa.GaussianBlur((0, 1.0)),
-                                    # blur images with a sigma between 0 and 3.0
-                                    iaa.AverageBlur(k=(3, 5)),
-                                    # blur image using local means with kernel sizes between 2 and 7
-                                    iaa.MedianBlur(k=(3, 5)),
-                                    # blur image using local medians with kernel sizes between 2 and 7
-                            ]),
-                            iaa.Sharpen(alpha=(0, 1.0), lightness=(0.9, 1.1)),
-                            # sharpen images
-                            iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)),
-                            # emboss images
-                            # search either for all edges or for directed edges,
-                            # blend the result with the original image using a blobby mask
-                            iaa.SimplexNoiseAlpha(iaa.OneOf([
-                                    iaa.EdgeDetect(alpha=(0.5, 1.0)),
-                                    iaa.DirectedEdgeDetect(alpha=(0.5, 1.0),
-                                                            direction=(0.0, 1.0)),
-                            ])),
-                            iaa.AdditiveGaussianNoise(loc=0,
-                                                        scale=(0.0, 0.01 * 255),
-                                                        per_channel=0.5),
-                            # add gaussian noise to images
-                            iaa.OneOf([
-                                    iaa.Dropout((0.01, 0.05), per_channel=0.5),
-                                    # randomly remove up to 10% of the pixels
-                                    iaa.CoarseDropout((0.01, 0.03),
-                                                        size_percent=(0.01, 0.02),
-                                                        per_channel=0.2),
-                            ]),
-                            iaa.Invert(0.01, per_channel=True),
-                            # invert color channels
-                            iaa.Add((-2, 2), per_channel=0.5),
-                            # change brightness of images (by -10 to 10 of original value)
-                            iaa.AddToHueAndSaturation((-1, 1)),
-                            # change hue and saturation
-                            # either change the brightness of the whole image (sometimes
-                            # per channel) or change the brightness of subareas
-                            iaa.OneOf([
-                                    iaa.Multiply((0.9, 1.1), per_channel=0.5),
-                                    iaa.FrequencyNoiseAlpha(
-                                            exponent=(-1, 0),
-                                            first=iaa.Multiply((0.9, 1.1),
-                                                                per_channel=True),
-                                            second=iaa.ContrastNormalization(
-                                                    (0.9, 1.1))
-                                    )
-                            ]),
-                            sometimes(iaa.ElasticTransformation(alpha=(0.5, 3.5),
-                                                                sigma=0.25)),
-                            # move pixels locally around (with random strengths)
-                            sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.05))),
-                            # sometimes move parts of the image around
-                            sometimes(iaa.PerspectiveTransform(scale=(0.01, 0.1)))
-                        ],
-                        random_order=True
-                        )
                 ],
                 random_order=True
         )
@@ -197,7 +138,7 @@ def net_architecture(path_to_model, base_model_trainable, classes):
         model.load_weights(path_to_model)
     return(model)
 
-def pascal_experiment(experiment_desc, experiment, path, batch_size, epochs, base_model_trainable, path_to_model, l2_regularization, learning_rate, augment):
+def pascal_experiment(experiment_desc, experiment, path, batch_size, epochs, base_model_trainable, path_to_model, l2_regularization, learning_rate, augment, scale, translate_percent, rotate, shear):
 
     if not os.path.exists(experiment_desc):
         os.makedirs(experiment_desc)
@@ -219,7 +160,7 @@ def pascal_experiment(experiment_desc, experiment, path, batch_size, epochs, bas
     labels = {key: value for (key, value) in zip(list(data), [x.split("_")[0] for x in list(data)])}
 
     # Generators
-    training_generator = DataGenerator(partition['train'], labels, path, augment=True, **params)
+    training_generator = DataGenerator(partition['train'], labels, path, augment = augment, scale = scale, translate_percent = translate_percent, rotate = rotate, shear = shear, **params)
     validation_generator = DataGenerator(partition['validation'], labels, path, **params)
 
     # Design model
@@ -282,6 +223,11 @@ def pascal_experiment(experiment_desc, experiment, path, batch_size, epochs, bas
         print("path_to_model: {}".format(path_to_model), file=text_file)
         print("learning_rate: {}".format(learning_rate), file=text_file)
         print("l2_regularization: {}".format(l2_regularization), file=text_file)
+        print("augment: {}".format(augment), file=text_file)
+        print("scale: {}".format(scale), file=text_file)
+        print("translate_percent: {}".format(translate_percent), file=text_file)
+        print("rotate: {}".format(rotate), file=text_file)
+        print("shear: {}".format(shear), file=text_file)
         print("", file=text_file)
         
         print("### Model - compilation ###", file=text_file)
